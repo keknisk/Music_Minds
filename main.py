ffeb4fcdbda79b50b85aa4a36c28288355e1e6b2
kedar <kedarkeknis@gmail.com>
2020-11-28 17:49:16 +0530

    "Added gitignore, trying to fetch image from flask to html, changed carousel"

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from PIL import Image
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+# im1 = Image.open("static/images/album.jpg")
+# im2 = Image.open("static/images/album2.jpg")
+# im3 = Image.open("static/images/album3.jpg")
+
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    im1 = Image.open("static/images/album.jpg")
+    im2 = Image.open("static/images/album2.jpg")
+    im3 = Image.open("static/images/album3.jpg")
+
+    return render_template('index.html', image = im1)
 
 
 
