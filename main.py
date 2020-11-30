d30e1bd2e5d99e7984c295f1ec4980547e5d9bf8
kedar <kedarkeknis@gmail.com>
2020-11-30 18:12:45 +0530

    "Stoping the project"

--- main.py
+++ main.py
@@ -4,23 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# im1 = Image.open("static/images/album.jpg")
-# im2 = Image.open("static/images/album2.jpg")
-# im3 = Image.open("static/images/album3.jpg")
-
-
-
 @app.route('/')
 def home():
-    im1 = Image.open("static/images/album.jpg")
-    im2 = Image.open("static/images/album2.jpg")
-    im3 = Image.open("static/images/album3.jpg")
-
-    return render_template('index.html', image = im1)
-
-
-
-
+    return render_template("index.html")
 
 
 app.run(debug=True)
