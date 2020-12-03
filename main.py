2ff1eb2a089be2d7ecab94285d5a4b97f8605be4
kedar <kedarkeknis@gmail.com>
2020-12-03 17:43:22 +0530

    "started project again"

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from PIL import Image
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/test1'
+db = SQLAlchemy(app)
+
+class Test(db.Model):
+    image = db.Column(db.BLOB)
+    backgr = db.Column(db.BLOB)
+    id = db.Column(db.Integer)
+    name = db.column(db.Integer)
+
 
 
 @app.route('/')
